0f026e0500128c4df738b6fc8f161367a9b5ed11
jhoanjg <jhoan@dataico.com>
2025-02-19 16:31:39 -0500

    finish proyect

--- src/main/java/org/example/pong/Main.java
+++ src/main/java/org/example/pong/Main.java
@@ -7,12 +7,12 @@ import com.googlecode.lanterna.input.KeyStroke;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TerminalDisplay t = new TerminalDisplay();
-        Ball b = new Ball(new Vector(3, 5), new Vector(-1, 6));
+        Ball b = new Ball(new Vector(3, 5), new Vector(30, 6));
         TerminalSize size = t.getTerminalSize();
         int height = size.getRows();
         int width = size.getColumns();
-        Paddle playerOne = new Paddle(new Vector(2, height / 2.0), 1);
-        Paddle playerTwo = new Paddle(new Vector(width - 2, height / 2.0), 1);
+        Paddle playerOne = new Paddle(new Vector(2, height / 2.0), 5);
+        Paddle playerTwo = new Paddle(new Vector(width - 2, height / 2.0), 5);
 
         double fps = 30.0;
         double timeDelta = 1.0 / fps;
@@ -28,8 +28,10 @@ public class Main {
             playerOne.draw(t);
             playerTwo.draw(t);
 
-            if (b.position.x >= width || b.position.x <= 0)
-                b.bounce(Axis.X);
+            if (b.position.x >= width || b.position.x <= 0) {
+                System.out.println("You lose!! :(");
+                System.exit(1);
+            }
 
             if (b.position.y >= height || b.position.y <= 0)
                 b.bounce(Axis.Y);
@@ -42,27 +44,26 @@ public class Main {
             if (k != null)
                 switch (k.getCharacter()) {
                     case 'w':
-                        playerOne.moveUp(t);
+                        playerOne.moveUp();
                         break;
                     case 's':
                         playerOne.moveDown(t);
                         break;
                     case 'k':
-                        playerTwo.moveUp(t);
+                        playerTwo.moveUp();
                         break;
                     case 'j':
                         playerTwo.moveDown(t);
                         break;
                 }
-//
-            System.out.println(b.position.x);
+
             t.setCursorPosition((int) Math.round(b.position.x), (int) Math.round(b.position.y));
 
             t.flush();
             long end = System.currentTimeMillis();
             long timeToRender = end - start;
 
-            Thread.sleep(timeDeltaMs - timeToRender);
+            Thread.sleep(Math.max(0, timeDeltaMs - timeToRender));
 
 
         }
--- src/main/java/org/example/pong/Paddle.java
+++ src/main/java/org/example/pong/Paddle.java
@@ -41,24 +41,19 @@ public class Paddle {
     }
 
     public void draw(TerminalDisplay t) {
-        t.putCharacter((int) Math.round(this.position.x), (int) Math.round(this.position.y), '|');
-        t.flush();
+        for (int y = (int) getTopY(); y <= getBottomY(); y++) {
+            t.putCharacter((int) Math.round(this.position.x), y, '|');
+        }
     }
 
-    public void moveUp(TerminalDisplay t) {
-        int height = t.getTerminalSize().getRows();
-        while (this.position.y < height) {
-            t.putCharacter((int) Math.round(this.position.x), (int) Math.round(this.position.y) - 1, '|');
-            t.flush();
-        }
+    public void moveUp() {
+        if (position.y > height / 2)
+            position.y--;
     }
 
     public void moveDown(TerminalDisplay t) {
-        while (this.position.y > 0) {
-            t.putCharacter((int) Math.round(this.position.x), (int) Math.round(this.position.y) + 1, '|');
-            t.flush();
-        }
+        int maxY = t.getTerminalSize().getRows();
+        if (getBottomY() < maxY)
+            position.y++;
     }
-
-
 }
