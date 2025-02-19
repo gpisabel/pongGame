be2d851dd67367b5bd62ae02e77d994608751b7d
dataicomacbook8 <davidjimenezch20@gmail.com>
2025-02-19 12:13:50 -0500

    Clean Automation loop

--- src/main/java/org/example/pong/Main.java
+++ src/main/java/org/example/pong/Main.java
@@ -7,27 +7,34 @@ import com.googlecode.lanterna.input.KeyStroke;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TerminalDisplay t = new TerminalDisplay();
-        Ball b = new Ball(new Vector(3, 5), new Vector(-1, 3));
+        Ball b = new Ball(new Vector(3, 5), new Vector(-1, 6));
         TerminalSize size = t.getTerminalSize();
         int height = size.getRows();
         int width = size.getColumns();
 
-        double timeDelta = 0.10;
+        double fps = 30.0;
+        double timeDelta = 1.0/fps;
         final long timeDeltaMs = (long) (timeDelta * 1000);
+        System.out.println(timeDeltaMs);
         KeyStroke k;
 
         do {
+            long start = System.currentTimeMillis();
             b.updatePosition(timeDelta);
+            t.clear();
+
 
             if (b.position.x >= width || b.position.x <= 0)
                 b.bounce(Axis.X);
 
             if (b.position.y >= height || b.position.y <= 0)
                 b.bounce(Axis.Y);
-            t.putCharacter((int) Math.round(b.position.x), (int) Math.round(b.position.y), '*');
+            t.setCursorPosition((int) Math.round(b.position.x), (int) Math.round(b.position.y) );
 
             t.flush();
-            Thread.sleep(timeDeltaMs);
+            long end = System.currentTimeMillis();
+            long timeToRender = end-start;
+            Thread.sleep(timeDeltaMs - timeToRender);
             k = t.getNextKeypress();
 
         } while (k == null || k.getCharacter() != 'q');
